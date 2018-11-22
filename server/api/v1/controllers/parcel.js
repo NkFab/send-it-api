c9371d7ad2abe30367d03a8e32ea8d2bd5ad1373
NkFab <fabjeezy@gmail.com>
2018-11-22 18:46:04 +0200

    [chore #162158516] Add class and methods in controllers for parcel

--- server/api/v1/controllers/parcel.js
+++ server/api/v1/controllers/parcel.js
@@ -1,52 +1,52 @@
 import Parcel from '../models/parcel';
 
-const getAllParcels = (req, res, next) => {
-    Parcel.search({})
-        .then(parcels => {
-            res.status(200).json({ parcels });
-        })
-        .catch(err => res.status(500).json({ ...err }));
-};
+export default class ParcelCont {
 
-const getParcelById = (req, res, next) => {
-    const { id } = req.params;
-    Parcel.searchById(id)
-        .then(parcel => {
-            if (parcel) {
-                res.status(200).json({ ...parcel });
-            } else {
-                res.status(404).json({ message: `Parcel ${id} is not found` });
-            }
-        })
-        .catch(err => res.status(500).json({ ...err }));
-};
-
-const getParcelOrderByUser = (req, res, next) => {
-    res.status(200).json({ message: "These were ordered by a user" });
-};
-
-const cancelParcelOrder = (req, res) => {
-    const { id } = req.params;
-    Parcel.update(id, { ...req.body })
-        .then(parcels => {
-            if (parcels) {
+    static getAllParcels = (req, res, next) => {
+        Parcel.search({})
+            .then(parcels => {
                 res.status(200).json({ parcels });
-            } else {
-                res.status(404).json({ message: `Parcel ${id} is not found` });
-            }
-        })
-        .catch(err => res.status(500).json({ ...err }));
-};
+            })
+            .catch(err => res.status(500).json({ ...err }));
+    };
 
-const createParcelOrder = (req, res, next) => {
-    const { recipient, ...rest } = req.body;
-    Parcel.create({ recipient, ...rest })
-        .then(parcels => {
-            res.status(201).json({ message: 'The parcel order was created', parcels });
-        })
-        .catch(err => res.status(500).json({ ...err }));
+    static getParcelById(req, res, next) {
+        const { id } = req.params;
+        Parcel.searchById(id)
+            .then(parcel => {
+                if (parcel) {
+                    res.status(200).json({ ...parcel });
+                } else {
+                    res.status(404).json({ message: `Parcel ${id} is not found` });
+                }
+            })
+            .catch(err => res.status(500).json({ ...err }));
+    };
+
+    static getParcelOrderByUser(req, res, next) {
+        res.status(200).json({ message: "These were ordered by a user" });
+    };
+
+    static cancelParcelOrder(req, res) {
+        const { id } = req.params;
+        Parcel.update(id, { ...req.body })
+            .then(parcels => {
+                if (parcels) {
+                    res.status(200).json({ parcels });
+                } else {
+                    res.status(404).json({ message: `Parcel ${id} is not found` });
+                }
+            })
+            .catch(err => res.status(500).json({ ...err }));
+    };
+
+    static createParcelOrder(req, res, next) {
+        const { recipient, ...rest } = req.body;
+        Parcel.create({ recipient, ...rest })
+            .then(parcels => {
+                res.status(201).json({ message: 'The parcel order was created', parcels });
+            })
+            .catch(err => res.status(500).json({ ...err }));
+    }
 }
-
-export { getAllParcels, getParcelById, getParcelOrderByUser, cancelParcelOrder, createParcelOrder };
-
 
