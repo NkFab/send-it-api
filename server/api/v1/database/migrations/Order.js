fc9e9a2c06f05c5e674771b9a7e725eaf939759b
NkFab <fabjeezy@gmail.com>
2018-11-27 02:01:58 +0200

    [chore #162152119] Add jwt and passport dependencies

--- server/api/v1/database/migrations/Order.js
+++ server/api/v1/database/migrations/Order.js
@@ -3,16 +3,19 @@ import env from "../../config/environment";
 const Order =
   `CREATE TABLE IF NOT EXISTS 
   orders(
-    order_no SERIAL PRIMARY KEY,
+    order_ref SERIAL PRIMARY KEY,
     origin VARCHAR(20) NOT NULL,
     destination VARCHAR(20) NOT NULL,
     beneficiary VARCHAR(20) NOT NULL,
     pick_date TIMESTAMP NOT NULL,
     price INT NOT NULL,
+    name_parcel VARCHAR(50) NOT NULL,
+    weight VARCHAR(128) NOT NULL,
+    description VARCHAR(128),
+    status CHAR(20) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    user_id INT references users,
-    ref INT references parcels
+    user_id INT references users
   )`;
 
 
--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import passport from "passport";
 import middlewares from './middlewares';
-
+import passportStategy from "./middlewares/auth/passport"
 const server = express();
 //in express 4.* >= body parser has been added in express and called using methods json
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(morgan('combined'));
 middlewares(server);
+server.use(passport.initialize());
+// passportStategy(passport);
+
 
 const port = 3000 || 8080;
 server.listen(port, err => {
